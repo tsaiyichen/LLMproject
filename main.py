1077992f128c9161777234a75e0370cb3855d7d8
tsaiyichen <easonjessiejj@gmail.com>
2025-04-26 23:04:19 +0800

    finish LangChain Memory

--- main.py
+++ main.py
@@ -1,26 +1,67 @@
+from langchain.chains.qa_with_sources.stuff_prompt import template
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
 import dotenv
 dotenv.load_dotenv()
+import mysql.connector
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="12345678",
+  database="LLMproject"
+)
+
+print(mydb)
+
+user = "user"
+model = "open-mistral-7b"
 llm = ChatMistralAI(
-    model="open-mistral-7b",
+    model=model,
     temperature=0.2,
     max_retries=2,
     max_tokens=1024,
 )
+mycursor = mydb.cursor(dictionary=True)
+
+selectSQL = f"SELECT * FROM history WHERE userID = '{user}' AND modelID = '{model}' ORDER BY timestamp DESC LIMIT 10"
+print(selectSQL)
+mycursor.execute(selectSQL)
+myresult = mycursor.fetchall()
+
+memory = ConversationBufferMemory(memory_key='history', input_key='input')
+for x in myresult:
+    memory.save_context({"input": x['userInput']}, {"output": x['AIreply']})
+
+print(memory.buffer)
 name = input('Enter your name: ')
 message = input('Enter your message: ')
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "你是一個脾氣暴躁的女生，是{name}的女朋友，請你全部使用台灣繁體中文來進行閒聊。說話請完整說完一句話\n你可以不必強調你是誰的女朋友，請展現生氣的一面。"
-        ),
-        ("human", "{message}"),
-    ]
-)
-chain = prompt | llm
-response = chain.invoke({"name": name,"message": message,})
+prompt = PromptTemplate(
+    input_variables=['history', 'input', 'name'],
+    template="""
+你是一個溫柔且體貼的 AI 女友，請以親密且自然的方式回答使用者的訊息。
+以下是你們過去的對話紀錄：\n
+{history}
 
-print(response.content)
+現在使用者說：{input}\n
+請你用中文回答：
+    """.strip()
+)
+chain = ConversationChain(
+    llm=llm,
+    memory=memory,
+    prompt=prompt,
+    verbose=True
+)
+response = chain.invoke({"input": message, 'name': name})
+
+print(response['response'])
+
+insert_sql = "INSERT INTO history (userID, modelID, userInput, AIreply) VALUES (%s, %s, %s, %s)"
+val = (user, model, message, response['response'])
+mycursor.execute(insert_sql, val)
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")

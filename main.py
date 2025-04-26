dc6147dcceddaffd083e44083e1de7e6735720b8
tsaiyichen <easonjessiejj@gmail.com>
2025-04-27 03:38:08 +0800

    finish FAISS

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from langchain.chains import ConversationChain
 import dotenv
 dotenv.load_dotenv()
 import mysql.connector
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.schema import Document
+import os
+from datetime import datetime
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -12,8 +17,6 @@ mydb = mysql.connector.connect(
   password="12345678",
   database="LLMproject"
 )
-
-print(mydb)
 
 user = "user"
 model = "open-mistral-7b"
@@ -23,10 +26,19 @@ llm = ChatMistralAI(
     max_retries=2,
     max_tokens=1024,
 )
+embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+db_path = "faiss_db"
+
+if os.path.exists(db_path):
+    db = FAISS.load_local(db_path, embedding_model, allow_dangerous_deserialization=True)
+else:
+    db = FAISS.from_texts(["這是初始化的資料，請忽略"], embedding_model)
+    db.save_local(db_path)
+
+
 mycursor = mydb.cursor(dictionary=True)
 
 selectSQL = f"SELECT * FROM history WHERE userID = '{user}' AND modelID = '{model}' ORDER BY timestamp DESC LIMIT 10"
-print(selectSQL)
 mycursor.execute(selectSQL)
 myresult = mycursor.fetchall()
 
@@ -48,6 +60,9 @@ prompt = PromptTemplate(
 請你用中文回答：
     """.strip()
 )
+retrieved_docs = db.similarity_search(message, k=5)
+retrieved_text = "\n".join([doc.page_content for doc in retrieved_docs])
+memory.save_context({"input": '(記憶檢索的資料)'}, {"output": retrieved_text})
 chain = ConversationChain(
     llm=llm,
     memory=memory,
@@ -64,3 +79,18 @@ mycursor.execute(insert_sql, val)
 mydb.commit()
 
 print(mycursor.rowcount, "record inserted.")
+
+new_content = f"Human: {message}\nAI: {response['response']}"
+new_doc = Document(
+    page_content=new_content,
+    metadata={
+        "userID": user,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 你可以自己加上 timestamp
+    }
+)
+
+# 新增進 FAISS
+db.add_documents([new_doc])
+print(f"目前FAISS裡面有 {db.index.ntotal} 筆資料")
+# 更新本地儲存（重要，避免重啟後消失）
+db.save_local(db_path)
